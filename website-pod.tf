4d7798133a1ae0b2a84ddd56430437b432466e0f
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-12-21 12:44:35 -0800

    Upgrade website-pod module to 2.5
    The 2.5 version changes how DNS records are created.
    See https://github.com/infrahouse/terraform-aws-website-pod/pull/17

--- website-pod.tf
+++ website-pod.tf
@@ -8,7 +8,7 @@ module "pod" {
     aws     = aws
     aws.dns = aws.dns
   }
-  version                               = "~> 2.4"
+  version                               = "~> 2.5"
   service_name                          = var.service_name
   environment                           = var.environment
   alb_name_prefix                       = substr(var.service_name, 0, 6)
