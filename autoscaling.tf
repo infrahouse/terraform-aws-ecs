952b5f116b30026871a46beddd1624159ba92337
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-08-16 14:07:45 -0700

    Add tests for autoscaling_metric

--- autoscaling.tf
+++ autoscaling.tf
@@ -5,9 +5,12 @@ resource "aws_appautoscaling_target" "ecs_target" {
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
-
+locals {
+  lb_arn_parts = split("/", module.pod.load_balancer_arn)
+  tg_arn_parts = split("/", module.pod.target_group_arn)
+}
 resource "aws_appautoscaling_policy" "ecs_policy" {
-  name               = "cpu-auto-scaling"
+  name               = "auto-scaling"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
@@ -16,8 +19,16 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
       predefined_metric_type = var.autoscaling_metric
+      resource_label = var.autoscaling_metric == "ALBRequestCountPerTarget" ? join(
+        "/", [
+          "app", local.lb_arn_parts[2], local.lb_arn_parts[3],
+          "targetgroup", local.tg_arn_parts[1], local.tg_arn_parts[2]
+        ]
+      ) : null
     }
-    target_value       = var.autoscaling_metric == "ECSServiceAverageCPUUtilization" ? var.autoscaling_target_cpu_usage : var.autoscaling_target
+    target_value = var.autoscaling_metric == "ECSServiceAverageCPUUtilization" ? (
+      var.autoscaling_target == null ? var.autoscaling_target_cpu_usage : var.autoscaling_target
+    ) : var.autoscaling_target
     scale_in_cooldown  = 300
     scale_out_cooldown = 300
   }
---
+++ test_data/httpd_autoscaling/datasources.tf
@@ -0,0 +1,37 @@
+data "aws_caller_identity" "this" {}
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_route53_zone" "cicd" {
+  name = var.test_zone
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-${var.ubuntu_codename}-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "state"
+    values = [
+      "available"
+    ]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
