61d1fc7bbb0196eb2b99cf927233dd8e8420e9ed
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-09-03 16:55:45 -0700

    Upgrade website-pod to 4.0.0 (#49)
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

--- website-pod.tf
+++ website-pod.tf
@@ -4,7 +4,7 @@ data "aws_key_pair" "ssh_key_pair" {
 
 module "pod" {
   source  = "registry.infrahouse.com/infrahouse/website-pod/aws"
-  version = "3.4.0"
+  version = "4.0.0"
   providers = {
     aws     = aws
     aws.dns = aws.dns
