2785fa29788d2a848d0632e9369c91a3686fda29
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-03-20 18:02:12 -0700

    style: fix black 26.3 findings

--- tests/test_httpd.py
+++ tests/test_httpd.py
@@ -14,9 +14,7 @@ from tests.conftest import (
 )
 
 
-@pytest.mark.parametrize(
-    "aws_provider_version", ["~> 6.0"], ids=["aws-6"]
-)
+@pytest.mark.parametrize("aws_provider_version", ["~> 6.0"], ids=["aws-6"])
 def test_module(
     service_network,
     keep_after,
