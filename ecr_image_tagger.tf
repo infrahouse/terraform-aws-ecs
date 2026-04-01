955c7052796a8c629ac8b3b23d0cbc60006826f9
xOtanix <maksym@tinyfish.io>
2026-04-01 19:22:24 +0300

    feat: pass memory_size to pass to ecr image tagger

--- ecr_image_tagger.tf
+++ ecr_image_tagger.tf
@@ -8,6 +8,7 @@ module "ecr_image_tagger" {
   handler           = "main.lambda_handler"
   lambda_source_dir = "${path.module}/assets/ecr_image_tagger"
   timeout           = 120
+  memory_size       = var.ecr_image_tagger_memory_size
 
   environment_variables = {
     ECS_CLUSTER_NAME    = aws_ecs_cluster.ecs.name
